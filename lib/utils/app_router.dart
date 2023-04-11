634040c2576a48e099774a3e90f19809e955e02c
Cromuel Oliver <cromuelbarut@gmail.com>
2023-04-11 19:03:17 +0800

    configured router for navigation, qr generator and qr scanner page

--- lib/utils/app_router.dart
+++ lib/utils/app_router.dart
@@ -1,15 +1,46 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../pages/pages.dart';
 
 class AppRouter {
+  static final GlobalKey<NavigatorState> _rootNavigator =
+      GlobalKey<NavigatorState>(debugLabel: 'root');
+  static final GlobalKey<NavigatorState> _shellNavigator =
+      GlobalKey<NavigatorState>(debugLabel: 'shell');
+
   static final GoRouter router = GoRouter(
+    navigatorKey: _rootNavigator,
     initialLocation: SplashPage.path,
     routes: [
       GoRoute(
         path: SplashPage.path,
         name: SplashPage.name,
         builder: (ctx, state) => const SplashPage(),
+      ),
+      ShellRoute(
+        navigatorKey: _shellNavigator,
+        builder: (BuildContext ctx, GoRouterState state, Widget child) {
+          return NavigationPage(child: child);
+        },
+        routes: [
+          GoRoute(
+            path: QRGeneratorPage.path,
+            name: QRGeneratorPage.name,
+            // builder: (ctx, state) => const QRGeneratorPage(),
+            pageBuilder: (ctx, state) {
+              return const NoTransitionPage(child: QRGeneratorPage());
+            },
+          ),
+          GoRoute(
+            path: QRScannerPage.path,
+            name: QRScannerPage.name,
+            // builder: (ctx, state) => const QRScannerPage(),
+            pageBuilder: (ctx, state) {
+              return const NoTransitionPage(child: QRScannerPage());
+            },
+          ),
+        ],
       ),
     ],
   );
