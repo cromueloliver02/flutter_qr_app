ba51c9e89c0e1893ea2738041c1e63afc3b7533b
Cromuel Oliver <cromuelbarut@gmail.com>
2023-04-11 19:21:03 +0800

    created bottom nav bar functionality

--- lib/pages/navigation/components/custom_bottom_navigation_bar.dart
+++ lib/pages/navigation/components/custom_bottom_navigation_bar.dart
@@ -1,23 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class CustomBottomNavigationBar extends StatelessWidget {
+import '../../pages.dart';
+
+class CustomBottomNavigationBar extends StatefulWidget {
   const CustomBottomNavigationBar({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
+  State<CustomBottomNavigationBar> createState() =>
+      _CustomBottomNavigationBarState();
+}
 
+class _CustomBottomNavigationBarState extends State<CustomBottomNavigationBar> {
+  int _currentIdx = 0;
+
+  void _onTap(BuildContext ctx, {required int idx}) {
+    setState(() => _currentIdx = idx);
+
+    switch (idx) {
+      case 0:
+        ctx.goNamed(QRGeneratorPage.name);
+        break;
+      case 1:
+        ctx.goNamed(QRScannerPage.name);
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return BottomNavigationBar(
-      items: [
+      currentIndex: _currentIdx,
+      onTap: (int idx) => _onTap(context, idx: idx),
+      items: const [
         BottomNavigationBarItem(
           label: 'QR Generator',
-          icon: const Icon(Icons.qr_code),
-          activeIcon: Icon(Icons.qr_code, color: theme.primaryColor),
+          icon: Icon(Icons.qr_code),
+          activeIcon: Icon(Icons.qr_code),
         ),
         BottomNavigationBarItem(
           label: 'QR Scanner',
-          icon: const Icon(Icons.qr_code_2),
-          activeIcon: Icon(Icons.qr_code_2, color: theme.primaryColor),
+          icon: Icon(Icons.qr_code_2),
+          activeIcon: Icon(Icons.qr_code_2),
         ),
       ],
     );
